8ff553f91ffc3b36ced23dc98d96f398e5b62645
Asif-Abdullah01 <asifabdullah214365@gmail.com>
2026-07-14 22:54:41 +0600

    introduction to variable

--- first.js
+++ first.js
@@ -1,1 +1,17 @@
 console.log(7)
+
+// variable
+
+var age = 10;
+var temp = 5;
+
+console.log(age);
+
+
+// let (pore change kra jabe), const (not changable)
+
+let price = 100;
+
+price = 100*.8;
+
+console.log(price);
