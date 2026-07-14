cf3fbc964a9fe53f5e14321a27ea3e929123c3b9
Asif-Abdullah01 <asifabdullah214365@gmail.com>
2026-07-14 23:42:35 +0600

    types of variable

--- first.js
+++ first.js
@@ -15,3 +15,21 @@ let price = 100;
 price = 100*.8;
 
 console.log(price);
+
+let name = "Asif";
+
+console.log(name)
+
+let learning1 = "My name is Asif"
+let learning2 = 'My name is Asif'
+
+console.log(learning1)
+console.log(learning2)
+
+
+const isRich = true;
+
+console.log(isRich)
+console.log(typeof isRich)
+console.log(typeof price)
+console.log(typeof name)
